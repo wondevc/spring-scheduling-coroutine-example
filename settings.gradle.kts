92dca6cfb69f2aabb8e6b6681fbd201dc379fcad
채지원 <112457971+wondevc@users.noreply.github.com>
2024-12-16 21:55:00 +0900

    feat: add spring scheduling coroutine module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,7 @@
 rootProject.name = "spring-scheduling-coroutine-example"
+
+include(
+    "spring-scheduling-coroutine",
+)
+
+project(":spring-scheduling-coroutine").projectDir = file("application/spring-scheduling-coroutine")
